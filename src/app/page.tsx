cc54164ee75069c090c56bae2258f2cc063d3365
rachel <rachel.uiux@gmail.com>
2024-01-12 00:35:18 +0900

    init: next.js, 프론트엔드 환경설정
    - install: husky, nextUI
    - eslint, prettier 설정

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from '@nextui-org/button';
+
+export default function Home() {
+  return (
+    <>
+      <div>Home</div>
+      <Button>동기 동기</Button>
+    </>
+  );
+}
