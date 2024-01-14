0503aa68b247fed50bdce353287b11e25ea5f47d
rachel <rachel.uiux@gmail.com>
2024-01-14 19:15:51 +0900

    refactor: home 쓸데없는 코드 정리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import { Button } from '@nextui-org/button';
 
-const Home = () => (
-  <>
-    <div>Home</div>
-    <Button>동기 동기</Button>
-  </>
-);
+const Home = () => <div>Home</div>;
 
 export default Home();
