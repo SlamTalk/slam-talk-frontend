fea3b2cd4ec66fb86550ec02b1ca0268010feee3
swimmingRiver <swimmingr@gmail.com>
2024-01-18 07:30:24 +0900

    feat: tag ui 추가

--- src/app/community/[tag]/layout.tsx
+++ src/app/community/[tag]/layout.tsx
@@ -7,8 +7,8 @@ const Layout = () => (
     <input placeholder="검색어를 입력해주세요" />
     <Link href="/community/all">전체</Link>/
     <Link href="/community/free">자유</Link>/
-    <Link href="/community/question">질문</Link>/
     <Link href="/community/usedtrade">중고 거래</Link>/
+    <Link href="/community/question">질문</Link>/
     <Link href="/community/rentaltransfer">대관 양도</Link>/
     <button type="button">
       <Link href="/community/write">+글 작성하기</Link>
--- src/app/community/write/page.tsx
+++ src/app/community/write/page.tsx
@@ -1,14 +1,27 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState, ChangeEvent, useMemo } from 'react';
+import { Key, Selection } from '@react-types/shared';
+
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+} from '@nextui-org/react';
 
 const Page = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [tag, setTag] = useState('');
+  const [selectedKeys, setSelectedKeys] = useState<Set<string>>(
+    new Set(['자유'])
+  );
 
   const [testList, setTestList] = useState<
-    { title: string; content: string }[]
+    { title: string; content: string; tag: string }[]
   >([]); // test code
 
   const router = useRouter();
@@ -18,14 +31,28 @@ const Page = () => {
   const contentHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setContent(e.target.value);
   };
-
+  const tagHandler = (key: Key) => {
+    console.log({ tag });
+    setTag(key.toString());
+  };
+  const selectedValue = useMemo(
+    () => Array.from(selectedKeys).join(', ').replaceAll('_', ' '),
+    [selectedKeys]
+  );
+  const handleSelectionChange = (keys: Selection) => {
+    setSelectedKeys(new Set<string>(Array.from(keys).map(String)));
+  };
   const submitHandler = () => {
     setTitle('');
     setContent('');
-    const storeData: { title: string; content: string } = { title, content };
+    const storeData: { title: string; content: string; tag: string } = {
+      title,
+      content,
+      tag,
+    };
 
     setTestList((prev) => [...prev, storeData]);
-    console.log({ testList });
+
     localStorage.setItem('community', JSON.stringify(testList)); // test code
 
     router.push('/community/all');
@@ -39,6 +66,35 @@ const Page = () => {
         value={content}
         onChange={contentHandler}
       />
+      <Dropdown>
+        <DropdownTrigger>
+          <Button variant="bordered">{selectedValue}</Button>
+        </DropdownTrigger>
+        <DropdownMenu
+          onAction={tagHandler}
+          aria-label="Static Actions"
+          selectionMode="single"
+          selectedKeys={selectedKeys}
+          onSelectionChange={handleSelectionChange}
+        >
+          <DropdownItem key="자유" value="free">
+            자유
+          </DropdownItem>
+          <DropdownItem
+            key="중고 거래
+"
+            value="usedtrade"
+          >
+            중고 거래
+          </DropdownItem>
+          <DropdownItem key="질문" value="question">
+            질문
+          </DropdownItem>
+          <DropdownItem key="대관 양도" value="rentaltransfer">
+            대관 양도
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
       <button type="button" onClick={submitHandler}>
         작성완료
       </button>
