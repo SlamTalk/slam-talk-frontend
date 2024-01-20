6a96b65d3cf02dfb529749dbac0a450d8addc363
rachel <rachel.uiux@gmail.com>
2024-01-20 21:17:38 +0900

    style: 다크/라이트 모드 반영, 반응형 작업

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,7 +4,7 @@ import { PiChatsCircle, PiMapPinBold } from 'react-icons/pi';
 import { AiOutlineHome } from 'react-icons/ai';
 
 const Footer = () => (
-  <div className="footer fixed bottom-0 z-30 h-[48px] w-full border-t-1 bg-white pt-[4px]">
+  <div className="footer fixed bottom-0 z-30 h-[48px] w-full border-t-1 bg-background pt-[4px]">
     <div className="flex items-center justify-around">
       <div className="grid place-items-center">
         <AiOutlineHome size={24} />
