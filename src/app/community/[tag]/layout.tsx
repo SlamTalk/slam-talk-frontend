ed3c1c2fca6616521c0321da327644fb001d2c63
swimmingRiver <swimmingr@gmail.com>
2024-01-24 16:18:24 +0900

    style: 검색창 위치변경

--- src/app/community/[tag]/layout.tsx
+++ src/app/community/[tag]/layout.tsx
@@ -14,6 +14,19 @@ const Layout = () => {
   const pathname = usePathname();
   return (
     <div className="flex flex-col">
+      <div className="z-10 flex w-full transform items-center justify-center rounded-md bg-background p-1 shadow-md">
+        <input
+          placeholder="검색어를 입력해주세요"
+          className="w-11/12 p-2 focus:outline-none focus:ring-0"
+        />
+        <button
+          type="button"
+          aria-label="search button in community"
+          className="ml-2 mr-2 flex h-full w-8 items-center justify-center rounded-md focus:outline-none"
+        >
+          <IoSearchSharp className="w-full text-gray-400 hover:text-black" />
+        </button>
+      </div>
       <div className="flex justify-center space-x-10">
         <Tabs aria-label="Options" selectedKey={pathname}>
           <Tab key="all" title="전체" href="/community/all" />
@@ -28,19 +41,6 @@ const Layout = () => {
         </Tabs>
       </div>
 
-      <div className="z-10 flex w-full transform items-center justify-center rounded-md bg-background p-1 shadow-md">
-        <input
-          placeholder="검색어를 입력해주세요"
-          className="w-11/12 p-2 focus:outline-none focus:ring-0"
-        />
-        <button
-          type="button"
-          aria-label="search button in community"
-          className="ml-2 mr-2 flex h-full w-8 items-center justify-center rounded-md focus:outline-none"
-        >
-          <IoSearchSharp className="w-full text-gray-400 hover:text-black" />
-        </button>
-      </div>
       <Page />
       <div className="fixed bottom-14 w-full max-w-[600px]">
         <div className="mr-4 flex justify-end">
