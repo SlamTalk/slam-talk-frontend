a1919eec0269d34c9baf0eb169a30efce5e035a8
swimmingRiver <swimmingr@gmail.com>
2024-01-28 23:49:58 +0900

    feat: input section 생성

--- src/app/chatting/page.tsx
+++ src/app/chatting/page.tsx
@@ -1,5 +1,20 @@
+import { Button, Input } from '@nextui-org/react';
+import { IoIosSend } from 'react-icons/io';
+
 import React from 'react';
 
-const chatting = () => <div />;
+const chatting = () => (
+  <div>
+    <div
+      aria-label="chat input section"
+      className="flex w-full rounded-lg border border-slate-300"
+    >
+      <Input className="" />
+      <Button isIconOnly className="h-auto w-14 border-none bg-transparent">
+        <IoIosSend className="text-4xl text-primary" />
+      </Button>
+    </div>
+  </div>
+);
 
 export default chatting;
