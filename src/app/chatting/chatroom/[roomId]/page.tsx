a1ef27707675a691f08ce5ac749d9787a477be11
swimmingRiver <swimmingr@gmail.com>
2024-01-29 12:07:19 +0900

    feat: chatroom rommId  라우터설정

--- src/app/chatting/chatroom/[roomId]/page.tsx
+++ src/app/chatting/chatroom/[roomId]/page.tsx
@@ -4,12 +4,13 @@ import { IoIosSend } from 'react-icons/io';
 import React from 'react';
 
 const chatting = () => (
-  <div>
+  <div aria-label="chatt room wrapper" className="h-[765px]">
+    <div className="h-[705px]">chatlist</div>
     <div
       aria-label="chat input section"
       className="flex w-full rounded-lg border border-slate-300"
     >
-      <Input className="" />
+      <Input className="text-xl" />
       <Button isIconOnly className="h-auto w-14 border-none bg-transparent">
         <IoIosSend className="text-4xl text-primary" />
       </Button>
