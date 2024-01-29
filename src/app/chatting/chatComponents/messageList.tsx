05d4e81c11bb4b604e3877df7a470180646f0380
swimmingRiver <swimmingr@gmail.com>
2024-01-29 17:51:29 +0900

    feat: userId에 따라 메시지 위치 변경

--- src/app/chatting/chatComponents/messageList.tsx
+++ src/app/chatting/chatComponents/messageList.tsx
@@ -8,19 +8,30 @@ const MessageList = () => {
     { id: 3, userId: '231', nickname: 'user-1', message: '잘가요~' },
   ];
   return (
-    <div className="h-[705px]">
-      {messages.map((i) => (
-        <div key={i.id} className="flex h-20 w-[150px]">
-          <div aria-label="userIcon">
-            <p>{i.nickname}</p>
-            <Avatar className="me-2 border-3 border-primary" />
+    <div className="h-[705px] w-full border border-slate-400">
+      {messages.map((i) =>
+        i.userId === '123' ? (
+          <div key={i.id} className="flex h-20 w-full justify-start">
+            <div aria-label="userIcon">
+              <p>{i.nickname}</p>
+              <Avatar className="me-2 ms-2 border-3 border-primary" />
+            </div>
+            <div className="my-4 max-w-sm rounded-lg bg-primary p-4 text-white">
+              {i.message}
+            </div>
           </div>
-          <div className="z-1 mx-auto my-4 max-w-sm rounded-lg bg-primary p-4 text-white">
-            {i.message}
-            <div className="relative bottom-2 right-3 z-0 h-5 w-4 -rotate-[22deg] border-t-0 border-solid border-blue-500 bg-primary" />
+        ) : (
+          <div key={i.id} className="flex h-20 w-full justify-end">
+            <div className="right-10 my-4 max-w-sm rounded-lg bg-primary p-4 text-white">
+              {i.message}
+            </div>
+            <div aria-label="userIcon">
+              <p>{i.nickname}</p>
+              <Avatar className="me-2 ms-2 border-3 border-primary" />
+            </div>
           </div>
-        </div>
-      ))}
+        )
+      )}
     </div>
   );
 };
