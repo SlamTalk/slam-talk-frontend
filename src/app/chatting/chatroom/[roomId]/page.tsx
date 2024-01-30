7139e73a956b456bd8c036936724f106c48581a9
swimmingRiver <swimmingr@gmail.com>
2024-01-30 17:12:45 +0900

    소켓 연결확인

--- src/app/chatting/chatroom/[roomId]/page.tsx
+++ src/app/chatting/chatroom/[roomId]/page.tsx
@@ -1,8 +1,33 @@
+'use client';
+
 import { Button, Input } from '@nextui-org/react';
 import { IoIosSend } from 'react-icons/io';
 
 import React from 'react';
+import * as StompJs from '@stomp/stompjs';
 import MessageList from '../../chatComponents/messageList';
+
+const chatTest = () => {
+  console.log('chat test start');
+  const client = new StompJs.Client({
+    brokerURL: `ws://localhost:8080/ws/slamtalk`,
+    connectHeaders: {
+      id: 'sub-0',
+      authorization:
+        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwiYXV0aCI6IlJPTEVfVVNFUiIsImV4cCI6MTcwNjg5MjgzN30.jAF5XPCuOvYOg0ZoKFhSG5zUi9o398mz9En9wqNNWpg2ubA3wFvYSSStWWKowO2cuOV2I0rbDzM4SZB86YP9nw',
+    },
+    debug: (msg) => {
+      console.log({ msg });
+    },
+    reconnectDelay: 5000,
+    heartbeatIncoming: 4000,
+    heartbeatOutgoing: 4000,
+  });
+  client.onStompError = function (frame) {
+    console.log({ frame });
+  };
+  client.activate();
+};
 
 const chatting = () => (
   <div aria-label="chatt room wrapper" className="h-[765px]">
@@ -12,7 +37,11 @@ const chatting = () => (
       className="flex w-full rounded-lg border border-slate-300"
     >
       <Input className="text-xl" />
-      <Button isIconOnly className="h-auto w-14 border-none bg-transparent">
+      <Button
+        isIconOnly
+        className="h-auto w-14 border-none bg-transparent"
+        onClick={chatTest}
+      >
         <IoIosSend className="text-4xl text-primary" />
       </Button>
     </div>
