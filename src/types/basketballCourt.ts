d66d329940e9bb9ee768501bd27b24e42f7781b1
rachel <rachel.uiux@gmail.com>
2024-02-06 21:06:27 +0900

    refactor: 쿼리 함수 위치, 이름 변경
    - BasketballCourt 타입 수정 예정

---
+++ src/types/basketballCourt.ts
@@ -0,0 +1,20 @@
+export interface BasketballCourt {
+  courtId: number;
+  courtName: string;
+  address: string;
+  latitude: number;
+  longitude: number;
+  // courtType: string;
+  // indoorOutdoor: string;
+  // courtSize: string;
+  // hoopCount: number;
+  // nightLighting: boolean;
+  // openingHours: boolean;
+  // fee: boolean;
+  // parkingAvailable: boolean;
+  // phoneNum: string | null;
+  // website: string | null;
+  // convenience: string[] | null;
+  // additionalInfo: string | null;
+  // photoUrl: string | null;
+}
