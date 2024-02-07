88cff48cf09ae328116611ed88c2e6fc86aec2ff
rachel <rachel.uiux@gmail.com>
2024-02-07 21:05:01 +0900

    refactor: 농구장 type 수정

--- src/services/basketballCourt/getBasketballCourts.ts
+++ src/services/basketballCourt/getBasketballCourts.ts
@@ -1,10 +1,10 @@
 import axiosInstance from '@/app/api/axiosInstance';
-import { BasketballCourt } from '@/types/basketballCourt';
+import { BasketballCourts } from '@/types/basketballCourt/basketballCourts';
 
 const getBasketballCourts = async () => {
   const result = await axiosInstance.get('/api/map/courts');
   const court = result.data.results;
-  const courtData: BasketballCourt = {
+  const courtData: BasketballCourts = {
     courtId: court.courtId,
     address: court.address,
     courtName: court.courtName,
