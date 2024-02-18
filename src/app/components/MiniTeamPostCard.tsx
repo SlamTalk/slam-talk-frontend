0dc4d098b3fff725308167e89da85ba10a14fd2c
Jiiker <motive46@naver.com>
2024-02-19 01:02:25 +0900

    feat: 매칭 페이지 최근 게시글 3개씩 보여주기

---
+++ src/app/components/MiniTeamPostCard.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Card, CardHeader, CardBody, Divider, Link } from '@nextui-org/react';
+import { TeamPost } from '@/types/matching/teamDataType';
+
+const MiniTeamPostCard = ({ teamPost }: { teamPost: TeamPost }) => {
+  const shortLocation = teamPost.locationDetail
+    .split(' ')
+    .slice(0, 2)
+    .join(' ');
+  const formattedDate = new Date(teamPost.scheduledDate).toLocaleDateString(
+    'ko-KR',
+    {
+      month: 'long',
+      day: 'numeric',
+    }
+  );
+
+  return (
+    <Link href={`/matching/team-details/${teamPost.teamMatchingId}`}>
+      <Card className="my-4 h-[180px] w-[180px]">
+        <CardHeader>
+          <div className="flex flex-col">
+            <p className="text-md font-bold">{shortLocation}</p>
+            <p className="text-small text-default-500">
+              {formattedDate} {teamPost.startTime}
+            </p>
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <p className="overflow-hidden">{teamPost.title}</p>
+        </CardBody>
+      </Card>
+    </Link>
+  );
+};
+export default MiniTeamPostCard;
