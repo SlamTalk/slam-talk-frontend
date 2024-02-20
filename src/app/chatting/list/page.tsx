833f7e485cb0253c75d8afe4217b606272d0a5af
swimmingRiver <swimmingr@gmail.com>
2024-02-21 03:20:52 +0900

    refactor: 테스트 코드 주석처리

--- src/app/chatting/list/page.tsx
+++ src/app/chatting/list/page.tsx
@@ -20,31 +20,31 @@ import LocalStorage from '@/utils/localstorage';
 
 import { useRouter } from 'next/navigation';
 import { IChatRoomListItem } from '../../../types/chat/chatRoomListItem';
-import axiosInstance from '../../api/axiosInstance';
-import { getUserData } from '../../../services/user/getUserData';
+// import axiosInstance from '../../api/axiosInstance';
+// import { getUserData } from '../../../services/user/getUserData';
 import { getChatList } from '../../../services/chatting/getChatList';
 
 const ChatList = () => {
   const router = useRouter();
   const isLoggedIn = LocalStorage.getItem('isLoggedIn');
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
-  const { data: loginData } = useQuery({
-    queryKey: ['loginData'],
-    queryFn: getUserData,
-  });
-  const createData = {
-    participants: [1, loginData?.id, 3],
-    roomType: 'TM',
-    togetherId: '1',
-    name: '함께 즐겨요~',
-  };
-  const postChatRoom = async () => {
-    const res = await axiosInstance.post(
-      `/api/chat/create`,
-      JSON.stringify(createData)
-    );
-    return res.data.results;
-  };
+  // const { data: loginData } = useQuery({
+  //   queryKey: ['loginData'],
+  //   queryFn: getUserData,
+  // });
+  // const createData = {
+  //   participants: [1, loginData?.id, 3],
+  //   roomType: 'TM',
+  //   togetherId: '1',
+  //   name: '함께 즐겨요~',
+  // };
+  // const postChatRoom = async () => {
+  //   const res = await axiosInstance.post(
+  //     `/api/chat/create`,
+  //     JSON.stringify(createData)
+  //   );
+  //   return res.data.results;
+  // };
   const { data: myChatList } = useQuery<IChatRoomListItem[]>({
     queryKey: ['myChatlist'],
     queryFn: getChatList,
@@ -101,7 +101,7 @@ const ChatList = () => {
           </div>
         </div>
       ))}
-      <div>
+      {/* <div>
         <button
           type="button"
           onClick={() => {
@@ -110,7 +110,7 @@ const ChatList = () => {
         >
           testroom
         </button>
-      </div>
+      </div> */}
     </div>
   ) : (
     <div>
