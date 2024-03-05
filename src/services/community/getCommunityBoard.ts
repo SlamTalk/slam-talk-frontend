a5f88314e3c2bc81da8db0144462c314149960ca
swimmingRiver <swimmingr@gmail.com>
2024-03-05 18:31:09 +0900

    feat: 게시글 최신 순으로 정렬

--- src/services/community/getCommunityBoard.ts
+++ src/services/community/getCommunityBoard.ts
@@ -3,6 +3,6 @@ import { IBoard } from '@/types/community/board';
 
 export const getCommunityBoard = async (): Promise<IBoard[]> => {
   const res = await axiosInstance('/api/community/board');
-  const communityBoard = res.data.results;
+  const communityBoard = res.data.results.reverse();
   return communityBoard;
 };
