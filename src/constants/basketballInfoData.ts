6c2844a7d82321d425a0b5e75de780f65800277f
Jiiker <motive46@naver.com>
2024-03-31 15:25:43 +0900

    refactor: 상수 데이터 정리(skill, position, location, scale)

---
+++ src/constants/basketballInfoData.ts
@@ -0,0 +1,52 @@
+export const basketballPositionUserData = [
+  {
+    value: '가드',
+    key: 'GUARD',
+  },
+  {
+    value: '포워드',
+    key: 'FORWARD',
+  },
+  { value: '센터', key: 'CENTER' },
+  { value: '무관', key: 'UNDEFINED' },
+  { value: '미정', key: 'UNSPECIFIED' },
+];
+
+export const basketballPositionApplyData = [
+  {
+    value: '가드',
+    key: 'GUARD',
+  },
+  {
+    value: '포워드',
+    key: 'FORWARD',
+  },
+  { value: '센터', key: 'CENTER' },
+  { value: '무관', key: 'UNSPECIFIED' },
+];
+
+export const basketballPositionFilterData = [
+  {
+    value: '가드',
+    key: 'GUARD',
+  },
+  {
+    value: '포워드',
+    key: 'FORWARD',
+  },
+  { value: '센터', key: 'CENTER' },
+];
+
+export const basketballSkillData = [
+  { value: '입문', key: 'BEGINER' },
+  { value: '하수', key: 'LOW' },
+  { value: '중수', key: 'MIDDLE' },
+  { value: '고수', key: 'HIGH' },
+];
+
+export const basketballGameScaleFilterData = [
+  { value: '2' },
+  { value: '3' },
+  { value: '4' },
+  { value: '5' },
+];
