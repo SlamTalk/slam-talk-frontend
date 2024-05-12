599a7a57944362f8701ad64213f8a4079497fcbe
hi-rachel <rachel.uiux@gmail.com>
2024-05-12 19:11:34 +0900

    feat: 비밀번호 규칙 특수문자 추가

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -6,7 +6,8 @@ export const validateNickname = (nickname: string) => {
 export const validateEmail = (email: string) =>
   /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i.test(email);
 
+/* eslint-disable no-useless-escape */
 export const validatePassword = (password: string) =>
-  /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,16}$/.test(
+  /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()\-_=+\[\]{};:'",.<>\/?])[A-Za-z0-9!@#$%^&*()\-_=+\[\]{};:'",.<>\/?]{8,16}$/.test(
     password
   );
