6c313765d09813d7eedb860ccd26319440c77a15
hi-rachel <rachel.uiux@gmail.com>
2024-05-12 18:26:08 +0900

    feat: 임시 비밀번호 발급 api 연결, 성공/실패 모달 처리

--- src/app/(auth)/find-password/page.tsx
+++ src/app/(auth)/find-password/page.tsx
@@ -1,7 +1,17 @@
 'use client';
 
+import axiosInstance from '@/app/api/axiosInstance';
 import { validateEmail } from '@/utils/validations';
-import { Button, Input } from '@nextui-org/react';
+import {
+  Button,
+  Input,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from '@nextui-org/react';
 import React, { useMemo, useState } from 'react';
 
 const FindPassword = () => {
@@ -11,15 +21,45 @@ const FindPassword = () => {
     [email]
   );
 
-  const handleSendEmail = () => {
-    alert(`비밀번호 찾기 구현 예정. 문의해주세요.`);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [msg, setMsg] = useState('');
+
+  const handleSendEmail = async () => {
+    if (email === '') {
+      setMsg('이메일을 입력해주세요.');
+      onOpen();
+      return;
+    }
+    try {
+      const response = await axiosInstance.patch(
+        '/api/user/temporary-passwords',
+        {
+          email,
+        }
+      );
+
+      if (response.status === 200) {
+        setMsg(
+          '해당 이메일로 임시 비밀번호 발송을 완료했습니다. 임시 비밀번호로 로그인을 진행하고 이후 마이페이지에서 비밀번호 변경을 진행해주세요.'
+        );
+        onOpen();
+      }
+    } catch (error) {
+      setMsg(
+        '비밀번호 재설정이 완료되지 않았습니다. 입력하신 이메일을 확인해주세요.'
+      );
+      onOpen();
+      console.log(error);
+    }
   };
 
   return (
     <>
       <title>슬램톡 | 비밀번호 찾기</title>
       <div className="mt-14 flex h-full w-full flex-col flex-wrap justify-center gap-3 p-5 align-middle md:flex-nowrap">
-        <h1 className="mb-4 text-2xl font-bold sm:text-xl">비밀번호 재설정</h1>
+        <h1 className="mb-4 text-2xl font-bold sm:text-xl">
+          임시 비밀번호 발급
+        </h1>
         <Input
           radius="sm"
           isClearable
@@ -29,7 +69,7 @@ const FindPassword = () => {
           label="이메일"
           value={email}
           onValueChange={setEmail}
-          placeholder="이메일"
+          placeholder="이메일 예) slamtalk@naver.com"
           isInvalid={isEmailInvalid}
         />
         <div
@@ -41,6 +81,23 @@ const FindPassword = () => {
           메일 발송
         </Button>
       </div>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="center">
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                비밀번호 찾기
+              </ModalHeader>
+              <ModalBody>{msg}</ModalBody>
+              <ModalFooter>
+                <Button color="primary" onPress={onClose}>
+                  확인
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </>
   );
 };
