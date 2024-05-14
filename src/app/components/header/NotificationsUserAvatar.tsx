87786285643bac35e19e1d1e74a825d546f35910
hi-rachel <rachel.uiux@gmail.com>
2024-05-14 19:02:24 +0900

    feat: AdminUserAvatar, NotificationsUserAvatar 스타일링 차이로 컴포넌트 분리

---
+++ src/app/components/header/NotificationsUserAvatar.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import React, { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { Avatar } from '@nextui-org/react';
+import { getOtherUserData } from '@/services/user/getOtherUserData';
+import UserProfile from '@/app/components/profile/UserProfile';
+import SmallLoading from '@/app/components/loading/SmallLoading';
+
+interface UserAvatarProps {
+  userId: number;
+}
+
+const NotificationsUserAvatar: React.FC<UserAvatarProps> = ({ userId }) => {
+  const { data: userData, isLoading } = useQuery({
+    queryKey: ['getUserData', userId],
+    queryFn: () => getOtherUserData({ userId }),
+  });
+
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+
+  const handleAvatarClick = () => {
+    setIsProfileOpen(true);
+  };
+
+  if (isLoading) {
+    return <SmallLoading />;
+  }
+
+  if (!userData) {
+    return null;
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === 'Enter') {
+      handleAvatarClick();
+    }
+  };
+
+  return (
+    <>
+      <div
+        aria-label="Avatar"
+        role="button"
+        onKeyDown={handleKeyDown}
+        tabIndex={0}
+        className="cursor-pointer"
+        onClick={handleAvatarClick}
+      >
+        <Avatar showFallback size="md" src={userData.imageUrl || ''} />
+      </div>
+      {isProfileOpen && (
+        <UserProfile
+          userId={userId}
+          isOpen={isProfileOpen}
+          onClose={() => setIsProfileOpen(false)}
+        />
+      )}
+    </>
+  );
+};
+
+export default NotificationsUserAvatar;
