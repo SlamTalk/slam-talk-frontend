75bb069c5f8ef9f0f23e323d846200de6b37c83a
hi-rachel <rachel.uiux@gmail.com>
2024-05-15 20:42:33 +0900

    style: 농구장 디테일 컴포넌트 한 줄 모두 items-center로 정렬

--- src/app/map/components/CourtDetails.tsx
+++ src/app/map/components/CourtDetails.tsx
@@ -195,7 +195,7 @@ const CourtDetails: React.FC<CourtDetailsProps> = ({
               <div className="flex justify-center " />
               <hr className="w-90 my-4 h-px bg-gray-300" />
               <div className="my-4 flex flex-col gap-4">
-                <div className="flex gap-2 align-middle">
+                <div className="flex items-center gap-2 align-middle">
                   <FaLocationDot
                     size={16}
                     className="dark:text-gray-20 text-gray-400"
@@ -205,7 +205,7 @@ const CourtDetails: React.FC<CourtDetailsProps> = ({
                     <span className="text-blue-500">복사</span>
                   </button>
                 </div>
-                <div className="flex gap-2 align-middle">
+                <div className="flex items-center gap-2 align-middle">
                   <FaClock
                     size={14}
                     className="text-gray-400 dark:text-gray-200"
@@ -217,7 +217,7 @@ const CourtDetails: React.FC<CourtDetailsProps> = ({
                     </span>
                   </span>
                 </div>
-                <div className="flex gap-2 align-middle">
+                <div className="flex items-center gap-2 align-middle">
                   <FaPhoneAlt
                     size={15}
                     className="pointer-events-auto text-gray-400 dark:text-gray-200"
@@ -227,14 +227,14 @@ const CourtDetails: React.FC<CourtDetailsProps> = ({
                     {selectedPlace.phoneNum ? selectedPlace.phoneNum : '-'}
                   </span>
                 </div>
-                <div className="flex gap-2 align-middle">
+                <div className="flex items-center gap-2 align-middle">
                   <FeeIcon className="text-gray-400 dark:text-gray-200" />
                   <span className="text-info text-blue-500">
                     이용료: {selectedPlace.fee}
                   </span>
                 </div>
 
-                <div className="flex gap-2 align-middle">
+                <div className="flex items-center gap-2 align-middle">
                   <WebsiteIcon className="text-gray-400 dark:text-gray-200" />
                   <span className="text-blue-500">
                     {selectedPlace.website ? (
@@ -246,32 +246,32 @@ const CourtDetails: React.FC<CourtDetailsProps> = ({
                     )}
                   </span>
                 </div>
-                <div className="flex gap-2 align-middle">
+                <div className="flex items-center gap-2 align-middle">
                   <CourtIcon className="text-gray-400 dark:text-gray-200" />
                   <span className="font-medium">
                     코트 종류: {selectedPlace.courtType}
                   </span>
                 </div>
-                <div className="flex gap-2 align-middle">
+                <div className="flex items-center gap-2 align-middle">
                   <CourtIcon className="text-gray-400 dark:text-gray-200" />
                   <span className="font-medium">
                     코트 사이즈: {selectedPlace.courtSize}
                   </span>
                 </div>
-                <div className="flex gap-2 align-middle">
+                <div className="flex items-center gap-2 align-middle">
                   <HoopIcon className="text-gray-400 dark:text-gray-200" />
                   <span className="font-medium">
                     골대 수: {selectedPlace.hoopCount}
                   </span>
                 </div>
-                <div className="flex gap-2 align-middle">
+                <div className="flex items-center gap-2 align-middle">
                   <FaLightbulb
                     size={17}
                     className="text-gray-400 dark:text-gray-200"
                   />
                   <span>야간 조명: {selectedPlace.nightLighting}</span>
                 </div>
-                <div className="flex gap-2 align-middle">
+                <div className="flex items-center gap-2 align-middle">
                   <FaParking
                     size={17}
                     className="text-gray-400 dark:text-gray-200"
@@ -279,7 +279,7 @@ const CourtDetails: React.FC<CourtDetailsProps> = ({
                   <span>주차: {selectedPlace.parkingAvailable}</span>
                 </div>
 
-                <div className="flex gap-2 align-middle text-sm">
+                <div className="flex items-center gap-2 align-middle text-sm">
                   <FaTag
                     size={17}
                     className="text-gray-400 dark:text-gray-200"
@@ -302,7 +302,7 @@ const CourtDetails: React.FC<CourtDetailsProps> = ({
                       )}
                   </ul>
                 </div>
-                <div className="flex gap-2 align-middle">
+                <div className="flex items-center gap-2 align-middle">
                   <InfoIcon className="text-gray-400 dark:text-gray-200" />
                   <span className="text-sm">
                     {selectedPlace.additionalInfo
