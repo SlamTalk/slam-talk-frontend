21bc0ead8df3febb68e025931334776f4ce1d7d9
hi-rachel <rachel.uiux@gmail.com>
2024-05-15 23:22:28 +0900

    feat: 로그인 페이지 footer 추가, 문의하기 추가

--- src/app/(auth)/login/layout.tsx
+++ src/app/(auth)/login/layout.tsx
@@ -18,12 +18,12 @@ const Loginlayout = ({ children }: { children: React.ReactNode }) => {
   };
 
   return (
-    <div className="md:h-100 sm:h-100 relative h-full w-full">
+    <div className="relative h-full w-full overflow-y-auto overflow-x-hidden">
       <div
         aria-label="뒤로가기"
         role="link"
         tabIndex={0}
-        className="absolute left-4 top-0"
+        className="absolute left-4 top-4"
         onClick={handleGoBack}
         onKeyDown={handleKeyDown}
       >
@@ -31,6 +31,14 @@ const Loginlayout = ({ children }: { children: React.ReactNode }) => {
       </div>
       <h2 className="mt-4 text-center text-lg font-semibold">로그인</h2>
       <main>{children}</main>
+      <footer className="sticky bottom-12 mt-72 flex w-full max-w-[600px] flex-col justify-center gap-2 bg-gray-100 px-4 pb-3 pt-8 text-xs text-gray-500">
+        <p className="text-sm">슬램톡 정보</p>
+        <a href="mailto:slamtalk.official@gmail.com">
+          <p>문의: slamtalk.official@gmail.com</p>
+        </a>
+        <hr className="my-3 h-px w-full bg-gray-300" />
+        <p className="text-gray-400">©Slam Talk. All rights reserved.</p>
+      </footer>
     </div>
   );
 };
